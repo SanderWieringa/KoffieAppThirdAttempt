8be5648d4374d0b2a2db2227c5dd246515379770
SanderWieringa <43203835+SanderWieringa@users.noreply.github.com>
2020-06-17 20:50:58 +0200

    Revert "changes"
    This reverts commit 7f10cac20d47f2917c72de2ad087527f8ebc05e7.

---
+++ src/main/resources/Static/src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import LoginPage from '../views/LoginPage.vue'
+import RegisterPage from '../views/RegisterPage.vue'
+import AddExpense from '../views/AddExpense.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', name: 'Home', component: Home },
+  { path: '/login', name: 'Login', component: LoginPage},
+  { path: '/register', name: 'Register',component: RegisterPage },
+  { path: '/AddExpense', name: 'AddExpense', component: AddExpense },
+];
+
+export const router = new VueRouter({
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  // redirect to login page if not logged in and trying to access a restricted page
+  const publicPages = ['/login', '/register']
+  const authRequired = !publicPages.includes(to.path)
+  const loggedIn = localStorage.getItem('user')
+
+  if (authRequired && !loggedIn) {
+    //return next('/login')
+    next()
+  } else {
+    next()
+  }
+}) 
+
