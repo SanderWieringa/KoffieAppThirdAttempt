8893a539a1f8f8dba68623e2f31d86229ef35930
SanderWieringa <43203835+SanderWieringa@users.noreply.github.com>
2020-06-17 20:45:36 +0200

    changes

--- src/main/java/Rest/Controllers/HomeController.java
+++ src/main/java/Rest/Controllers/HomeController.java
@@ -1,13 +1,11 @@
 package Rest.Controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-    @CrossOrigin
     @RequestMapping(value="")
     public String index(){
         return "index";
