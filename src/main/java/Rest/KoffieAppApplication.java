7f10cac20d47f2917c72de2ad087527f8ebc05e7
SanderWieringa <43203835+SanderWieringa@users.noreply.github.com>
2020-06-17 20:46:49 +0200

    changes

--- src/main/java/Rest/KoffieAppApplication.java
+++ src/main/java/Rest/KoffieAppApplication.java
@@ -8,8 +8,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.ArrayList;
 import java.util.logging.Logger;
@@ -46,18 +44,6 @@ public class KoffieAppApplication {
             //  log.info(bauer.toString());
             // }
             log.info("");
-        };
-    }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:3337");
-                registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:8080");
-
-            }
         };
     }
 }
